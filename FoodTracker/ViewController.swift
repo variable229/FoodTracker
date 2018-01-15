a2e37c0cb9bd92692522cececb6837d2a77fa47d
Tri Le <tri.le@me.com>
2018-01-14 19:21:58 -0800

    Build a Basic UI Completed

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,10 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//  Will not be using this section in the lesson, suggsted to remove but I commented out
+//
+//  override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
